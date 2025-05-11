007dfba02bcfecae1b610ae8e0e853b249540895
Vanessa Berardozzi <ness.berardozzi@gmail.com>
2025-05-11 22:32:25 +0200

    initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  printWidth: 80,
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: "avoid"
+};
